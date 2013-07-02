e4a8cb33fdb64b2b2e1c3cf7b8cb2fed9ee73315
Benjamin Peng <j100002ben@gmail.com>
2013-07-02 18:12:21 +0800

    Update justfont scripts to v2.1

--- application/views/share/modal_foot_scripts.blade.php
+++ application/views/share/modal_foot_scripts.blade.php
@@ -1,4 +1,4 @@
-<script src="{{ asset('js/justfont-app-v2.min.js') }}?t=20130619"></script>
+<script src="{{ asset('js/justfont-app-v2.min.js') }}?t=20130702"></script>
 <script>
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-39783774-2']);
