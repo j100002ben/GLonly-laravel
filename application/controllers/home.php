7e4f51df0dbb9fe5d92b4438ad5436648ca2cd6f
Benjamin Peng <j100002ben@gmail.com>
2013-07-17 21:45:21 +0800

    Add registration page.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -35,4 +35,26 @@ class Home_Controller extends Base_Controller {
 		return View::make('home.index');
 	}
 
+	public function action_registration()
+	{
+		$time = time();
+		$start_time = strtotime("2013-08-01 00:00:00 GMT+8");
+		$end_time = strtotime("2013-08-31 00:00:00 GMT+8");
+		if ( $time > $start_time AND $time < $end_time ) {
+			return Redirect::to('https://docs.google.com/forms/d/'.
+				'1lmKKaPaXntM9KEaDIUTjq2rlN_D4RFf_9mhBZ0kQhos/'.
+				'viewform?entry.1677042488&entry.1385487865&entry.'.
+				'1787948090&entry.505554217&entry.586857048&entry.'.
+				'1644652406=1&entry.916073990=1&entry.221816683&entry.'.
+				'225837570&entry.1318214892=%E5%90%8C%E4%BA%BA+/+'.
+				'%E4%BA%8C%E6%AC%A1%E5%89%B5%E4%BD%9C&entry.1802719544='.
+				'%E5%85%A8%E5%B9%B4%E9%BD%A1&entry.133011554='.
+				'%E6%9C%AA%E5%AE%9A&entry.785285632&entry.1133850059');
+		} else {
+			return View::make('home.wait_registration')
+				->with('start_time', $start_time)
+				->with('end_time', $end_time);
+		}
+	}
+
 }
---
+++ application/views/home/wait_registration.blade.php
@@ -0,0 +1,84 @@
+@include('share.doctype')
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+	<title>社團報名尚未開始 | 《百年好合》2014 百合Only | 'Love for All Seasons' 2014 Girls Love Festival</title>
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-touch-fullscreen" content="yes">
+	<link rel="shortcut icon" href="{{ asset('img/icon/favicon-64.png') }}" type="image/png">
+	<link rel="shortcut icon" type="image/png" href="{{ asset('img/icon/favicon-64.png') }}" />
+	<link href="{{ asset('css/registration.css') }}?t=20130717" rel="stylesheet">
+	<script type="text/javascript">var _jf = _jf || [];_jf.push(['_setAppId','e2510f16VwT5XHbraM7MKTaHR3qT2MdKu1jBGGtTppUF0K0R_uDa3ktbQ7mcNDOyCPJtSCY4MOtOxSWjB4pAJ8rT6ca9tUuIPML9PaiaaJPtH96PgsgSdZc8UlOyysfDDv8VwTLKRnzuQjDTRAKZntVGZvG3BX7k2-IZ5-qbaGQuGkL9-mo=']);_jf.push(['p','22617']);_jf.push(['_setFont','xingothic-tc-w8','css','.xingothic-tc-w8']);_jf.push(['_setFont','afupop08hollowthinframe','css','.afupop08hollowthinframe']);_jf.push(['_setFont','datd5','css','.datd5']);_jf.push(['_setFont','afu3dsuperblack','css','.afu3dsuperblack']);(function(f,q,c,h,e,i,r,d){var k=f._jf;if(k.constructor===Object){return}var l,t=q.getElementsByTagName("html")[0],a=function(u){for(var v in k){if(k[v][0]==u){if(false===k[v][1].call(k)){break}}}},j=/\S+/g,o=/[\t\r\n\f]/g,b=/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,g="".trim,s=g&&!g.call("\uFEFF\xA0")?function(u){return u==null?"":g.call(u)}:function(u){return u==null?"":(u+"").replace(b,"")},m=function(y){var w,z,v,u,x=typeof y==="string"&&y;if(x){w=(y||"").match(j)||[];z=t[c]?(" "+t[c]+" ").replace(o," "):" ";if(z){u=0;while((v=w[u++])){if(z.indexOf(" "+v+" ")<0){z+=v+" "}}t[c]=s(z)}}},p=function(y){var w,z,v,u,x=arguments.length===0||typeof y==="string"&&y;if(x){w=(y||"").match(j)||[];z=t[c]?(" "+t[c]+" ").replace(o," "):"";if(z){u=0;while((v=w[u++])){while(z.indexOf(" "+v+" ")>=0){z=z.replace(" "+v+" "," ")}}t[c]=y?s(z):""}}},n;k.push(["_eventActived",function(){p(h);m(e)}]);k.push(["_eventInactived",function(){p(h);m(i)}]);k.addScript=n=function(u,A,w,C,E,B){E=E||function(){};B=B||function(){};var x=q.createElement("script"),z=q.getElementsByTagName("script")[0],v,y=false,D=function(){x.src="";x.onerror=x.onload=x.onreadystatechange=null;x.parentNode.removeChild(x);x=null;a("_eventInactived");B()};if(C){v=setTimeout(function(){D()},C)}x.type=A||"text/javascript";x.async=w;x.onload=x.onreadystatechange=function(G,F){if(!y&&(!x.readyState||/loaded|complete/.test(x.readyState))){y=true;if(C){clearTimeout(v)}x.src="";x.onerror=x.onload=x.onreadystatechange=null;x.parentNode.removeChild(x);x=null;if(!F){setTimeout(function(){E()},200)}}};x.onerror=function(H,G,F){if(C){clearTimeout(v)}D();return true};x.src=u;z.parentNode.insertBefore(x,z)};a("_eventPreload");m(h);n(r,"text/javascript",false,3000)})(this,this.document,"className","jf-loading","jf-active","jf-inactive","//ds.justfont.com/js/stable/v/2.2/id/78046988184");</script>
+</head>
+<body>
+	<div class="group">
+		<div id="center-image"></div>
+		<div id="header-inner" class="xingothic-tc-w8">
+			<h1>《百年好合》2014 百合Only</h1>
+			<h2>"Love for All Seasons" 2014 Girls Love Festival</h2>
+			<ul>
+			<li>2014.03.08</li>
+			<li>Y17台北市青少年育樂中心</li>
+			<li>社團報名期間：（滿額抽籤制）</li>
+			<li>2013.08.01 - 2013.08.31</li>
+			<li>報名流程圖：<a href="http://www.glonly.tw/img/circle-flow.png" target="_blank">連結（開新視窗）</a></li>
+			</ul>
+			<span style="display:none;">0123456789</span>
+		</div>
+	</div>
+	<p class="info">距離社團報名開始，還有：</p>
+	<div id="counter" class="counter xingothic-tc-w8">
+		<ul class="countdown">
+			<li><div class="countdown_num" id="countdown_day"></div><div>Days</div></li>
+			<li><div class="countdown_num" id="countdown_hour"></div><div>Hours</div></li>
+			<li><div class="countdown_num"  id="countdown_min"></div><div>Minutes</div></li>
+			<li><div class="countdown_num" id="countdown_sec"></div><div>Seconds</div></li>
+		</ul>
+	</div>
+<script type="text/javascript">
+
+(function(window, document, undefined){
+	var countdown_day = document.getElementById('countdown_day')
+	  , countdown_hour = document.getElementById('countdown_hour')
+	  , countdown_min = document.getElementById('countdown_min')
+	  , countdown_sec = document.getElementById('countdown_sec')
+	  , ddate = function (time1, time2) {
+	  		var seconds = time1 - time2;
+			return {
+				days: Math.floor( seconds / 86400 )
+			  , hours: Math.floor( ( seconds % 86400 ) / 3600 )
+			  , minutes: Math.floor( ( ( seconds % 86400 ) % 3600 ) / 60 )
+			  , seconds: ( ( seconds % 86400 ) % 3600 ) % 60
+			}
+		}
+	  , update = function (dd) {
+	  		countdown_day.innerHTML = dd.days;
+			countdown_hour.innerHTML = dd.hours;
+			countdown_min.innerHTML = dd.minutes;
+			countdown_sec.innerHTML = dd.seconds;
+		}
+	  , start = {{ $start_time }}
+	  , end = {{ $end_time }}
+	  , counter = function (){
+			var now = Math.round((new Date()).getTime() / 1000)
+			  , dd;
+			if ( now > start ) {
+				if ( now < end ) {
+					window.location.reload();
+				} else {
+					document.getElementById('counter').style.display = 'none';
+					// 社團報名時間已結束
+				}
+			} else {
+				update( ddate( start, now ) );
+				setTimeout(counter, 197);
+			}
+		}
+	  ;
+	_jf.push(['_eventActived',counter]);
+	_jf.push(['_eventInactived',counter]);
+})(this, this.document);
+</script>
+</body>
+</html>
