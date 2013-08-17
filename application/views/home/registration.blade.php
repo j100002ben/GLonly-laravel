e079410d6b2d09af614b07c3883e4feefe78988b
Benjamin Peng <j100002ben@gmail.com>
2013-08-18 02:16:55 +0800

    add language for regiatration page.

--- application/views/home/registration.blade.php
+++ application/views/home/registration.blade.php
@@ -10,11 +10,23 @@
 	<link rel="shortcut icon" type="image/png" href="{{ asset('img/icon/favicon-64.png') }}" />
 	<link href="{{ asset('css/registration.css') }}?t=20130717" rel="stylesheet">
 	<script type="text/javascript">var _jf = _jf || [];_jf.push(['_setAppId','e2510f16VwT5XHbraM7MKTaHR3qT2MdKu1jBGGtTppUF0K0R_uDa3ktbQ7mcNDOyCPJtSCY4MOtOxSWjB4pAJ8rT6ca9tUuIPML9PaiaaJPtH96PgsgSdZc8UlOyysfDDv8VwTLKRnzuQjDTRAKZntVGZvG3BX7k2-IZ5-qbaGQuGkL9-mo=']);_jf.push(['p','22617']);_jf.push(['_setFont','xingothic-tc-w8','css','.xingothic-tc-w8']);_jf.push(['_setFont','afupop08hollowthinframe','css','.afupop08hollowthinframe']);_jf.push(['_setFont','datd5','css','.datd5']);_jf.push(['_setFont','afu3dsuperblack','css','.afu3dsuperblack']);(function(f,q,c,h,e,i,r,d){var k=f._jf;if(k.constructor===Object){return}var l,t=q.getElementsByTagName("html")[0],a=function(u){for(var v in k){if(k[v][0]==u){if(false===k[v][1].call(k)){break}}}},j=/\S+/g,o=/[\t\r\n\f]/g,b=/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,g="".trim,s=g&&!g.call("\uFEFF\xA0")?function(u){return u==null?"":g.call(u)}:function(u){return u==null?"":(u+"").replace(b,"")},m=function(y){var w,z,v,u,x=typeof y==="string"&&y;if(x){w=(y||"").match(j)||[];z=t[c]?(" "+t[c]+" ").replace(o," "):" ";if(z){u=0;while((v=w[u++])){if(z.indexOf(" "+v+" ")<0){z+=v+" "}}t[c]=s(z)}}},p=function(y){var w,z,v,u,x=arguments.length===0||typeof y==="string"&&y;if(x){w=(y||"").match(j)||[];z=t[c]?(" "+t[c]+" ").replace(o," "):"";if(z){u=0;while((v=w[u++])){while(z.indexOf(" "+v+" ")>=0){z=z.replace(" "+v+" "," ")}}t[c]=y?s(z):""}}},n;k.push(["_eventActived",function(){p(h);m(e)}]);k.push(["_eventInactived",function(){p(h);m(i)}]);k.addScript=n=function(u,A,w,C,E,B){E=E||function(){};B=B||function(){};var x=q.createElement("script"),z=q.getElementsByTagName("script")[0],v,y=false,D=function(){x.src="";x.onerror=x.onload=x.onreadystatechange=null;x.parentNode.removeChild(x);x=null;a("_eventInactived");B()};if(C){v=setTimeout(function(){D()},C)}x.type=A||"text/javascript";x.async=w;x.onload=x.onreadystatechange=function(G,F){if(!y&&(!x.readyState||/loaded|complete/.test(x.readyState))){y=true;if(C){clearTimeout(v)}x.src="";x.onerror=x.onload=x.onreadystatechange=null;x.parentNode.removeChild(x);x=null;if(!F){setTimeout(function(){E()},200)}}};x.onerror=function(H,G,F){if(C){clearTimeout(v)}D();return true};x.src=u;z.parentNode.insertBefore(x,z)};a("_eventPreload");m(h);n(r,"text/javascript",false,3000)})(this,this.document,"className","jf-loading","jf-active","jf-inactive","//ds.justfont.com/js/stable/v/2.2/id/78046988184");</script>
+	<!--[if IE 8]>
+		<script src="{{ asset('js/jquery-1.9.1.min.js') }}"></script>
+	<![endif]-->
+	<!--[if gte IE 9]><!--><script src="{{ asset('js/jquery-2.0.0.min.js') }}"></script><!--<![endif]-->
 </head>
 <body>
+	<div class="lang-ctrl">
+		Language 語言設定：
+		<select id="lang-select">
+			<option value="zh">中文</option>
+			<option value="en">English</option>
+			<option value="jp">日本語</option>
+		</select>
+	</div>
 	<div class="group">
 		<div id="center-image"></div>
-		<div id="header-inner" class="xingothic-tc-w8">
+		<div data-lang="zh" class="header-inner show xingothic-tc-w8">
 			<h1>《百年好合》2014 百合Only</h1>
 			<h2>"Love for All Seasons" 2014 Girls Love Festival</h2>
 			<ul>
@@ -26,10 +38,42 @@
 			</ul>
 			<span style="display:none;">0123456789</span>
 		</div>
+		<div data-lang="en" class="header-inner hide xingothic-tc-w8">
+			<h1>《百年好合》2014 百合Only</h1>
+			<h2>"Love for All Seasons" 2014 Girls Love Festival</h2>
+			<ul>
+			<li>2014.03.08</li>
+			<li>Y17台北市青少年育樂中心</li>
+			<li>Circle registration date: </li>
+			<li>2013.08.01 - 2013.08.31</li>
+			<li>( We’ll ballot if booths are full. )</li>
+			<li>Registration flow：<a href="http://i.imgur.com/GYuNOxI.png" target="_blank">Link (New Window)</a></li>
+			</ul>
+			<span style="display:none;">0123456789</span>
+		</div>
+		<div data-lang="jp" class="header-inner hide xingothic-tc-w8">
+			<h1>《百年好合》2014 百合Only</h1>
+			<h2>"Love for All Seasons" 2014 Girls Love Festival</h2>
+			<ul>
+			<li>2014.03.08</li>
+			<li>Y17台北市青少年育樂中心</li>
+			<li>申込期間：</li>
+			<li>2013.08.01 - 2013.08.31</li>
+			<li>（応募者多数の場合は抽選となります）</li>
+			<li>申込みの流れ：<a href="http://i.imgur.com/GYuNOxI.png" target="_blank">リンク</a></li>
+			</ul>
+			<span style="display:none;">0123456789</span>
+		</div>
 	</div>
-	<p class="regis-link">報名表單： <a target="_blank" href="http://bit.ly/glonly-regis">http://bit.ly/glonly-regis</a> （中文）</p>
-	<p class="regis-link">海外社團： <a target="_blank" href="http://bit.ly/glonly-regis-oversea">http://bit.ly/glonly-regis-oversea</a> （日本語 / English）</p>
-	<p class="info">距離社團報名截止，還有：</p>
+	<p data-lang="zh" class="regis-link">報名表單： <a target="_blank" href="http://bit.ly/glonly-regis">http://bit.ly/glonly-regis</a> （中文）</p>
+	<p data-lang="zh" class="regis-link">海外社團： <a target="_blank" href="http://bit.ly/glonly-regis-oversea">http://bit.ly/glonly-regis-oversea</a> （日本語 / English）</p>
+	<p data-lang="en" class="hide regis-link">Registration Form： <a target="_blank" href="http://bit.ly/glonly-regis">http://bit.ly/glonly-regis</a> （Chinese 中文）</p>
+	<p data-lang="en" class="hide regis-link">Form for Oversea： <a target="_blank" href="http://bit.ly/glonly-regis-oversea">http://bit.ly/glonly-regis-oversea</a> （日本語 / English）</p>
+	<p data-lang="jp" class="hide regis-link">申込表： <a target="_blank" href="http://bit.ly/glonly-regis">http://bit.ly/glonly-regis</a> （中文）</p>
+	<p data-lang="jp" class="hide regis-link">海外サークル様はこちら： <a target="_blank" href="http://bit.ly/glonly-regis-oversea">http://bit.ly/glonly-regis-oversea</a> （日本語 / English）</p>
+	<p data-lang="zh" class="info">距離社團報名截止，還有：</p>
+	<p data-lang="en" class="hide info">Days left from closing date：</p>
+	<p data-lang="jp" class="hide info">申込締切まで，あと：</p>
 	<div id="counter" class="counter xingothic-tc-w8">
 		<ul class="countdown">
 			<li><div class="countdown_num" id="countdown_day"></div><div>Days</div></li>
@@ -39,18 +83,18 @@
 		</ul>
 	</div>
 <script type="text/javascript">
-
 (function(window, document, undefined){
-	var countdown_day = document.getElementById('countdown_day')
-	  , countdown_hour = document.getElementById('countdown_hour')
-	  , countdown_min = document.getElementById('countdown_min')
-	  , countdown_sec = document.getElementById('countdown_sec')
+	var $ = window.jQuery
+	  , countdown_day = $('#countdown_day')
+	  , countdown_hour = $('#countdown_hour')
+	  , countdown_min = $('#countdown_min')
+	  , countdown_sec = $('#countdown_sec')
 	  , update = function (time1, time2) {
 	  		var seconds = time1 - time2;
-	  		countdown_day.innerHTML = Math.floor( seconds / 86400 );
-			countdown_hour.innerHTML = Math.floor( ( seconds % 86400 ) / 3600 );
-			countdown_min.innerHTML = Math.floor( ( ( seconds % 86400 ) % 3600 ) / 60 );
-			countdown_sec.innerHTML = ( ( seconds % 86400 ) % 3600 ) % 60;
+	  		countdown_day.text(Math.floor( seconds / 86400 ));
+			countdown_hour.text(Math.floor( ( seconds % 86400 ) / 3600 ));
+			countdown_min.text(Math.floor( ( ( seconds % 86400 ) % 3600 ) / 60 ));
+			countdown_sec.text(( ( seconds % 86400 ) % 3600 ) % 60);
 		}
 	  , start = {{ $start_time }}
 	  , end = {{ $end_time }}
@@ -70,6 +114,10 @@
 	  ;
 	_jf.push(['_eventActived',counter]);
 	_jf.push(['_eventInactived',counter]);
+	$('#lang-select').on('change', function(){
+		$('[data-lang]').removeClass('show').addClass('hide');
+		$('[data-lang='+$(this).val()+']').addClass('show');
+	});
 })(this, this.document);
 </script>
 </body>
